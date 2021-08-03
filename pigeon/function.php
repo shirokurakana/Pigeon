3fc133004896fb9bf9865c2b636f1006de449874
shirokurakana <admin@thdog.moe>
2021-08-03 16:35:32 +0800

    提升小头像分辨率

--- pigeon/function.php
+++ pigeon/function.php
@@ -133,7 +133,7 @@ class Pigeon {
 				}
 				$sdelete = ($rw[2] == $loginUser || $isAdmin) ? $delete : "";
 				$pstatus = $rw[4] == '2' ? "&nbsp;&nbsp;<code>仅自己可见</code>" : "";
-				$html .= "<tr><td class='headimg'><img src='https://gravatar.loli.net/avatar/" . md5($this->getUserInfo($rw[2])['email']) . "?s=64'</td><td class='thread'><p><small>{$rw[2]} 发表于" . $this->getDateFormat($rw[3]) . "&nbsp;&nbsp;<a href='?s=msg&id={$rw[0]}' target='_blank'><i class='fa fa-external-link'></i></a>" . $pstatus . str_replace("{id}", $rw[0], $sdelete) . "</small></p>";
+				$html .= "<tr><td class='headimg'><img src='https://gravatar.loli.net/avatar/" . md5($this->getUserInfo($rw[2])['email']) . "?s=128'</td><td class='thread'><p><small>{$rw[2]} 发表于" . $this->getDateFormat($rw[3]) . "&nbsp;&nbsp;<a href='?s=msg&id={$rw[0]}' target='_blank'><i class='fa fa-external-link'></i></a>" . $pstatus . str_replace("{id}", $rw[0], $sdelete) . "</small></p>";
 				$html .= "<div class='message'>" . $Markdown->text($rw[1]) . "</div></td></tr>";
 			}
 			if($i == 0) {
@@ -200,7 +200,7 @@ class Pigeon {
 				}
 			}
 			$pstatus = $rs['public'] == '2' ? "&nbsp;&nbsp;<code>仅自己可见</code>" : "";
-			$html .= "<tr><td class='headimg'><img src='https://gravatar.loli.net/avatar/" . md5($this->getUserInfo($rs['author'])['email']) . "?s=64'</td><td class='thread'><p><small>{$rs['author']} 发表于" . $this->getDateFormat($rs['time']) . $pstatus . str_replace("{id}", $id, $delete) . "</small></p>";
+			$html .= "<tr><td class='headimg'><img src='https://gravatar.loli.net/avatar/" . md5($this->getUserInfo($rs['author'])['email']) . "?s=128'</td><td class='thread'><p><small>{$rs['author']} 发表于" . $this->getDateFormat($rs['time']) . $pstatus . str_replace("{id}", $id, $delete) . "</small></p>";
 			$html .= "<div class='message'>" . $Markdown->text($rs['content']) . "</div></td></tr>";
 			$html .= "</table></div>";
 			return $html;
